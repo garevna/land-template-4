efaaf716bd1d75507eddac4c577bfb4d8800a775
Irina Fylyppova <irina.h.fylyppova@gmail.com>
2020-04-03 10:05:12 +0300

    icons paths in manifest

--- vue.config.js
+++ vue.config.js
@@ -17,11 +17,11 @@ module.exports = {
     themeColor: '#FF0E00',
     msTileColor: '#FEFEFE',
     iconPaths: {
-      favicon32: '/img/icons/favicon-32x32.png',
-      favicon16: '/img/icons/favicon-16x16.png',
-      appleTouchIcon: '/img/icons/apple-touch-icon.png',
-      maskIcon: '/img/icons/favicon.png',
-      msTileImage: '/img/icons/mstile-150x150.png'
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon.png',
+      maskIcon: 'img/icons/favicon.png',
+      msTileImage: 'img/icons/mstile-150x150.png'
     }
   },
   runtimeCompiler: true,
